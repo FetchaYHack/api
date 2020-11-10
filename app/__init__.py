77f9b41ec28f67e8e19a97f013a1ee7f3ea0dd44
ErikBoesen <me@erikboesen.com>
2020-11-09 22:30:42 -0700

    Remove celery

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from config import Config
-from celery import Celery
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,8 +7,6 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
 cors = CORS(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
